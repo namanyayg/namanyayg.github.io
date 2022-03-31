4c4885dd2a3af7ec2f1e3ff10181ba1ec9e4a72a
Namanyay Goel <mail@namanyayg.com>
2022-03-31 23:08:58 +0530

    responsive

--- front/build/webpack.dev.conf.js
+++ front/build/webpack.dev.conf.js
@@ -2,12 +2,11 @@
 const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
-const { merge } = require('webpack-merge')
+const merge = require('webpack-merge')
 const path = require('path')
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
@@ -16,42 +15,41 @@ const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
-    // rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
 
   // these devServer options should be customized in /config/index.js
   devServer: {
-    // clientLogLevel: 'warning',
+    clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
       ],
     },
     hot: true,
-    // contentBase: false, // since we use CopyWebpackPlugin.
+    contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
-    // overlay: config.dev.errorOverlay
-      // ? { warnings: false, errors: true }
-      // : false,
-    // publicPath: config.dev.assetsPublicPath,
+    overlay: config.dev.errorOverlay
+      ? { warnings: false, errors: true }
+      : false,
+    publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
-    // quiet: true, // necessary for FriendlyErrorsPlugin
-    // watchOptions: {
-    //   poll: config.dev.poll,
-    // }
+    quiet: true, // necessary for FriendlyErrorsPlugin
+    watchOptions: {
+      poll: config.dev.poll,
+    }
   },
   plugins: [
-    new VueLoaderPlugin(),
     new webpack.DefinePlugin({
       'process.env': require('../config/dev.env')
     }),
     new webpack.HotModuleReplacementPlugin(),
-    // new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
@@ -60,13 +58,13 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       inject: true
     }),
     // copy custom static assets
-    new CopyWebpackPlugin({
-      patterns: [{
+    new CopyWebpackPlugin([
+      {
         from: path.resolve(__dirname, '../static'),
         to: config.dev.assetsSubDirectory,
-        // ignore: ['.*']
-      }]
-    })
+        ignore: ['.*']
+      }
+    ])
   ]
 })
 
@@ -87,8 +85,8 @@ module.exports = new Promise((resolve, reject) => {
           messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
         onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
+          ? utils.createNotifierCallback()
+          : undefined
       }))
 
       resolve(devWebpackConfig)
--- front/src/components/Works/Works.vue
+++ front/src/components/Works/Works.vue
@@ -9,10 +9,10 @@
     .ventures
       .wrap
         .ventures__content
-          p Currently reinventing nutrition @ <a href="https://ayom.io/" class="ayom" rel="nofollow">Ayom</a> 🥬
-          p and leading web & blockchain development @ <a href="https://dyeus.co/" class="dyeus">Dyeus</a> 🌟
-          p &hellip;and a few more <em>exciting</em> products, currently in <span class="stealth">stealth mode</span> ‍🚀
-          p <em>while actively hiring: critical thinkers & achievers</em> 🧠
+          p Currently reinventing nutrition @ <a href="https://ayom.io/" class="ayom" rel="nofollow">Ayom</a>&nbsp;🥬
+          p and leading web & blockchain development @ <a href="https://dyeus.co/" class="dyeus">Dyeus</a>&nbsp;🌟
+          p &hellip;and a few more <em>exciting</em> products, currently in <span class="stealth">stealth mode</span>&nbsp;‍🚀
+          p <em>while actively hiring: critical thinkers & achievers</em>&nbsp;🧠
     //- .works
       Work(v-for="work in getWorks('featured')" :key="work" :data="works[work]")
 </template>
@@ -95,6 +95,7 @@ gradient-text($gradient, $bg = 'light')
 .ventures__content
   font-size 2em
   margin 0 0 9rem
+  line-height 2
 
   .ayom
     gradient-text('linear-gradient(to right, rgba(24,138,141,1) 11.2%, rgba(96,221,142,1))', 'light')
@@ -105,19 +106,37 @@ gradient-text($gradient, $bg = 'light')
   .stealth
     gradient-text('linear-gradient(to right,#7f9fa3, #aec4c7, #bedbdf, #7f9fa3, #b8d0d3)', 'light')
 
-@media (max-width 67.5em)
-  .header
-    flex-direction column
-    justify-content center
-    margin 0 0 2em
-    text-align center
+@media (max-width 82.5em)
+  .work
+    font-size .875em
 
-  .title
-    margin 0
+  .ventures__content
+    font-size 1.75em
+    line-height 2.25
+
+@media (max-width 67.5em)
+  .work
+    .header
+      flex-direction column
+      margin 0 0 2em
+      align-items start !important
+
+    .title
+      margin 0
+
+    .subtitle
+      font-size 1.375em
+
+  .ventures__content
+    font-size 1.5em
+    line-height 2.5
 
 @media (max-width 60em)
   .subtitle
     font-size 1.125em
+
+  .work
+    font-size .75em
 
 @media (max-width 50em)
   .title
@@ -125,4 +144,19 @@ gradient-text($gradient, $bg = 'light')
 
   .subtitle
     font-size 1em
+
+  .ventures__content
+    margin 0 0 6em
+
+@media (max-width 37.5em)
+  .ventures__content
+    line-height 1.5
+    font-size 1.375em
+
+  .work .work-intro
+    padding-bottom 1em
+
+@media (max-width 32.5em)
+  .ventures__content
+    font-size 1.25em
 </style>
